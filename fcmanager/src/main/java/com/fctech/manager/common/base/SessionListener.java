b34cc83827a1e32d39feae26cccf8abef7f5c0af
zhouzs1986 <zhouzs1986@gmail.com>
2015-12-19 17:43:25 +0800

    add login

--- fcmanager/src/main/java/com/fctech/manager/common/base/SessionListener.java
+++ fcmanager/src/main/java/com/fctech/manager/common/base/SessionListener.java
@@ -1,35 +1,31 @@
 package com.fctech.manager.common.base;
 
-import com.mysql.jdbc.log.LogFactory;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import java.util.Hashtable;
-import java.util.Iterator;
 
-import javax.servlet.http.HttpSession;
 import javax.servlet.http.HttpSessionBindingListener;
 import javax.servlet.http.HttpSessionEvent;
 import javax.servlet.http.HttpSessionListener;
 
-
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
- * Session监听器 完成对Seesion会话资源的实时监控
+ * Session鐩戝惉鍣�瀹屾垚瀵筍eesion浼氳瘽璧勬簮鐨勫疄鏃剁洃鎺�
  * 
  * @author weizhao.dong
  * @see HttpSessionBindingListener
  */
 public class SessionListener implements HttpSessionListener {
-	
+
 	private static Logger log = LoggerFactory.getLogger(SessionListener.class);
-	/*private static PSEssionService pSEssionService;*/
-	// 集合对象，保存session对象的引用
+	/* private static PSEssionService pSEssionService; */
+	// 闆嗗悎瀵硅薄锛屼繚瀛榮ession瀵硅薄鐨勫紩鐢�
 	static Hashtable ht = new Hashtable();
 
 	/**
-	 * 实现HttpSessionListener接口，完成session创建事件控制
-	 * 说明：此时的Session状态为无效会话，只有用户成功登录系统后才将此Session写入EAHTTPSESSION表作为有效SESSION来管理
+	 * 瀹炵幇HttpSessionListener鎺ュ彛锛屽畬鎴恠ession鍒涘缓浜嬩欢鎺у埗
+	 * 璇存槑锛氭鏃剁殑Session鐘舵�涓烘棤鏁堜細璇濓紝鍙
+	 * 湁鐢ㄦ埛鎴愬姛鐧诲綍绯荤粺鍚庢墠灏嗘Session鍐欏叆EAHTTPSESSION琛ㄤ綔涓烘湁鏁圫ESSION鏉ョ鐞�
 	 */
 	public void sessionCreated(HttpSessionEvent event) {
 	}
@@ -38,6 +34,5 @@ public class SessionListener implements HttpSessionListener {
 	public void sessionDestroyed(HttpSessionEvent se) {
 
 	}
-
 
 }
