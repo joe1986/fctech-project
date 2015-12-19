4d7528cdf9cd160d09d0d4d9e47b679cd2184914
zhouzs1986 <zhouzs1986@gmail.com>
2015-12-19 17:50:34 +0800

    add login

--- fcmanager/src/main/java/com/fctech/manager/common/controller/CommonController.java
+++ fcmanager/src/main/java/com/fctech/manager/common/controller/CommonController.java
@@ -1,6 +1,11 @@
 package com.fctech.manager.common.controller;
 
-import org.apache.log4j.spi.LoggerFactory;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.slf4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -8,38 +13,29 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.util.HashMap;
-import java.util.Map;
-
 /**
  * Created by joe on 15/10/4.
  */
 @Controller
 @RequestMapping
 public class CommonController extends BaseController {
-    Logger logger = org.slf4j.LoggerFactory.getLogger(CommonController.class);
-    private static Map<String, String> map = new HashMap<String, String>();
+	Logger logger = org.slf4j.LoggerFactory.getLogger(CommonController.class);
+	private static Map<String, String> map = new HashMap<String, String>();
 
-    static {
-        map.put("error", "/common/error");
-        map.put("500", "/common/500");
-        map.put("404", "/common/404");
-    }
+	static {
+		map.put("error", "/common/error");
+		map.put("500", "/common/500");
+		map.put("404", "/common/404");
+	}
 
-    @RequestMapping(value = "/home")
-    public ModelAndView login() {
-        return new ModelAndView("home");
-    }
-
-    @RequestMapping(value = "/error/{type}", method = RequestMethod.GET)
-    public ModelAndView dispatcher(HttpServletRequest request, HttpServletResponse response, @PathVariable String type) {
-        String mPage = "/user/home";
-        if (map.containsKey(type)) {
-            mPage = map.get(type);
-        }
-        return new ModelAndView(mPage);
-    }
+	@RequestMapping(value = "/error/{type}", method = RequestMethod.GET)
+	public ModelAndView dispatcher(HttpServletRequest request,
+			HttpServletResponse response, @PathVariable String type) {
+		String mPage = "/user/home";
+		if (map.containsKey(type)) {
+			mPage = map.get(type);
+		}
+		return new ModelAndView(mPage);
+	}
 
 }
--- fcmanager/src/main/java/com/fctech/manager/user/controller/LoginController.java
+++ fcmanager/src/main/java/com/fctech/manager/user/controller/LoginController.java
@@ -7,10 +7,11 @@ import org.slf4j.LoggerFactory;
 import org.springframework.context.MessageSource;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
-import com.alibaba.fastjson.JSON;
 import com.fctech.manager.common.base.AjaxResult;
+import com.fctech.manager.common.base.SessionManage;
 import com.fctech.manager.common.controller.BaseController;
 import com.fctech.manager.user.VO.UserInfoVO;
 import com.fctech.manager.user.exception.UserException;
@@ -32,14 +33,24 @@ public class LoginController extends BaseController {
 	@Resource
 	private ILoginService loginService;
 
+	@RequestMapping(value = "/index")
+	public ModelAndView index() {
+		if (SessionManage.getSessionUser(SessionManage.ADMIN_SESSION_KEY) != null) {
+			return home();
+		} else {
+			return new ModelAndView("login");
+		}
+
+	}
+
 	@RequestMapping(value = "/login")
-	public AjaxResult login(UserInfoVO vo) {
+	public @ResponseBody AjaxResult login(UserInfoVO vo) {
 		AjaxResult result = new AjaxResult();
 		try {
 			loginService.checkUserLogin(vo);
 		} catch (UserException e) {
 			result.setError(e.getErrorCode(), messageSource);
-			LOGGER.warn(JSON.toJSONString(result));
+			LOGGER.warn(result.toJson());
 		} catch (Exception e) {
 			result.setError(UserException.SYSTEM_ERROR, messageSource);
 			LOGGER.error(e.getMessage(), e);
@@ -48,8 +59,14 @@ public class LoginController extends BaseController {
 		return result;
 	}
 
+	@RequestMapping(value = "/home")
+	public ModelAndView home() {
+		return new ModelAndView("home");
+	}
+
 	@RequestMapping(value = "/logout")
 	public ModelAndView logout() {
+		SessionManage.loginOut(SessionManage.ADMIN_SESSION_KEY);
 		return new ModelAndView("login");
 	}
 
